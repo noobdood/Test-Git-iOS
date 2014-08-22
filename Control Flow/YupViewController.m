0a5cd3c30b8aba3df27a8d56f38929afc542e5e8
new <new@news-Mac-mini.local>
2014-08-22 15:39:24 -0400

    first commit

--- Control Flow/YupViewController.m
+++ Control Flow/YupViewController.m
@@ -18,6 +18,30 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    int truckSpeed = 45;
+    int lamboSpeed = 120;
+    int carSpeed = 80;
+    int mySpeed = carSpeed;
+    
+    // < > <
+    if (mySpeed < 70) {
+        NSLog(@"Keep Cruising");
+    }
+    else if (mySpeed > 55 && mySpeed < 90) {
+        NSLog(@"Cruising down highway 66");
+    }
+    else {
+        NSLog(@"Slow Down");
+    }
+    BOOL isTelevisionOn = YES;
+    
+    if (isTelevisionOn != YES) {
+        NSLog(@"we should take a break soon and do some");
+    }
+    else {
+        NSLog(@"great job keep up the good work");
+    }
 }
 
 - (void)didReceiveMemoryWarning
