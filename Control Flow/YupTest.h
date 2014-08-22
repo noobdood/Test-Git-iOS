4c6e0aceb424756f037ddfb1206cd69148dd029f
new <new@news-Mac-mini.local>
2014-08-22 15:56:34 -0400

    added a test class

---
+++ Control Flow/YupTest.h
@@ -0,0 +1,13 @@
+//
+//  YupTest.h
+//  Control Flow
+//
+//  Created by new on 8/22/14.
+//  Copyright (c) 2014 Yup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YupTest : NSObject
+
+@end
---
+++ Control Flow/YupTest.m
@@ -0,0 +1,13 @@
+//
+//  YupTest.m
+//  Control Flow
+//
+//  Created by new on 8/22/14.
+//  Copyright (c) 2014 Yup. All rights reserved.
+//
+
+#import "YupTest.h"
+
+@implementation YupTest
+
+@end
